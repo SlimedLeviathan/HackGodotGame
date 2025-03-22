532e5974d405ea8bf7d453945a98ea1c473c37b0
Kodi <144355274+kodizat@users.noreply.github.com>
2025-03-22 00:15:49 -0400

    Character created and spawns
    The character has been created with basic functionality and can spawn into the map. Collision is needed within the tiles.

---
+++ Scenes/character.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction for left and right movement.
+	# The "ui_left" and "ui_right" keys are mapped in the Input Map.
+	var direction := 0
+	if Input.is_action_pressed("ui_left"):
+		direction -= 1  # Move left
+	if Input.is_action_pressed("ui_right"):
+		direction += 1  # Move right
+
+	# Apply the movement in the X direction
+	velocity.x = direction * SPEED
+
+	# Handle deceleration when no keys are pressed
+	if direction == 0:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Move the character with collision detection.
+	move_and_slide()
--- Scenes/tile_map_layer.gd
+++ Scenes/tile_map_layer.gd
@@ -2,14 +2,26 @@ extends TileMapLayer
 
 var rng = RandomNumberGenerator.new()
 
+# Declare the variable for the character scene.
+var character_scene : PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#Load the character scene
+	#character_scene = preload("res://Scenes/Character.tscn")
+	
 	var levelFolder = DirAccess.open("res://Levels")
 	
 	var files = levelFolder.get_files()
 	
 	loadLevel(files[rng.randi_range(0,files.size() - 1)].split(".")[0])
 	#saveLevel("Level5", getLevelData())
+	
+	#Add the character to the scene at a specific position
+	#var character = character_scene.instantiate()
+	#character.position = Vector2(100, 100) 
+	#get_tree().root.add_child(character)
+	#self.get_parent().add_child(character)
 	
 	pass # Replace with function body.
 
